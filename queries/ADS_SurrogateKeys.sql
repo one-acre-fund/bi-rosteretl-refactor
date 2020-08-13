e418bfd48e8ec8464d23f11dc09c68712a53ac89
fmandela <fredrick.mandela@oneacrefund.org>
2020-08-13 11:57:28 +0300

    Update surrogate key queries

--- queries/ADS_SurrogateKeys.sql
+++ queries/ADS_SurrogateKeys.sql
@@ -14,42 +14,67 @@
 */
 
 /*
-	Update cls.staging_ADS
-		set DimCountryID=geo.CountryID,
-			DimRegionID=geo.RegionID,
-			DimDistrictID=geo.LocationID
-	from (
-		select CountryID, RegionID, LocationID
-		from OAF_SHARED_DIMENSIONS.dbo.DimLocations
-		where RosterDistrictID is not null 
-		) geo
-	where geo.RosterDistrictID=cls.staging_ADS.DistrictID
-	and cls.staging_ADS.DimCountryID is null
-
-
-	Update cls.staging_ADS
-		set DimSeasonID=s.SeasonID,
-	from (
-		select CountryID, SeasonID,RosterSeasonID
-		from OAF_SHARED_DIMENSIONS.dbo.DimSeasons 
-		where RosterSeasonID is not null  and isDeleted=0
-		) s
-	where s.RosterSeasonID=cls.staging_ADS.SeasonID
-	and s.CountryID=cls.staging_ADS.DimCountryID
-	and cls.staging_ADS.DimSeasonID is null
-
-
-	update cls.staging_ADS
-		set DimClientID=client.DimClientID,
-			GlobalClientID=client.GlobalClientID
-	from (
-		SELECT distinct c.ClientID,c.DistrictID,cs.ClientID DimClientID, cs.GlobalClientID
-		from OAF_SHARED_DIMENSIONS.dbo.DimRosterClient cs
-		join OAF_SHARED_DIMENSIONS_staging.dbo.Staging_Clients c on cs.GlobalClientID=c.GlobalClientID
-	) client
-	where client.ClientID=cls.staging_ADS.ClientID and client.DistrictID=cls.staging_ADS.DistrictID
-	and cls.staging_ADS.DimClientID is null
-
+Update
+    cls.staging_ADS
+set
+    DimCountryID = geo.CountryID,
+    DimRegionID = geo.RegionID,
+    DimDistrictID = geo.LocationID
+from
+    (
+        select
+            CountryID,
+            RegionID,
+            LocationID,
+            RosterDistrictID
+        from
+            OAF_SHARED_DIMENSIONS.dbo.DimLocations
+        where
+            RosterDistrictID is not null
+    ) geo
+where
+    geo.RosterDistrictID = cls.staging_ADS.RosterDistrictID
+    and cls.staging_ADS.DimCountryID is null
+Update
+    cls.staging_ADS
+set
+    DimSeasonID = s.SeasonID
+from
+    (
+        select
+            CountryID,
+            SeasonID,
+            RosterSeasonID
+        from
+            OAF_SHARED_DIMENSIONS.dbo.DimSeasons
+        where
+            RosterSeasonID is not null
+            and isDeleted = 0
+    ) s
+where
+    s.RosterSeasonID = cls.staging_ADS.RosterSeasonID
+    and s.CountryID = cls.staging_ADS.DimCountryID
+    and cls.staging_ADS.DimSeasonID is null
+update
+    cls.staging_ADS
+set
+    DimClientID = client.DimClientID,
+    GlobalClientID = client.GlobalClientID
+from
+    (
+        SELECT
+            distinct c.ClientID,
+            c.DistrictID,
+            cs.ClientID DimClientID,
+            cs.GlobalClientID
+        from
+            OAF_SHARED_DIMENSIONS.dbo.DimRosterClient cs
+            join OAF_SHARED_DIMENSIONS_staging.dbo.Staging_Clients c on cs.GlobalClientID = c.GlobalClientID
+    ) client
+where
+    client.ClientID = cls.staging_ADS.RosterClientID
+    and client.DistrictID = cls.staging_ADS.RosterDistrictID
+    and cls.staging_ADS.DimClientID is null
 */
 
 
--- queries/Repayment_SurrogateKeys.sql
+++ queries/Repayment_SurrogateKeys.sql
@@ -22,44 +22,78 @@
 
 /*
 
-	Update cls.staging_VerticalRepayments
-		set DimCountryID=geo.CountryID,
-			DimRegionID=geo.RegionID,
-			DimDistrictID=geo.DistrictID,
-			DimSectorID=geo.SectorID,
-			DimSiteID=geo.SiteID,
-			DimGroupID=geo.GroupID
-	from (
-		select CountryID, RegionID, DistrictID, SectorID, SiteID, GroupID, DistrictName,SiteName, GroupName
-		from OAF_SHARED_DIMENSIONS.dbo.DimGroup 
-		where isDeleted=0 
-		) geo
-	where geo.DistrictName=cls.staging_VerticalRepayments.District and geo.GroupName=staging_VerticalRepayments.GroupAtTimeOfRepayment
-	and cls.staging_VerticalRepayments.SiteAtTimeOfRepayment=geo.SiteName
-	and cls.staging_VerticalRepayments.DimCountryID is null
+Update
+    cls.staging_VerticalRepayments
+set
+    DimCountryID = geo.CountryID,
+    DimRegionID = geo.RegionID,
+    DimDistrictID = geo.DistrictID,
+    DimSectorID = geo.SectorID,
+    DimSiteID = geo.SiteID,
+    DimGroupID = geo.GroupID
+from
+    (
+        select
+            CountryID,
+            RegionID,
+            DistrictID,
+            SectorID,
+            SiteID,
+            GroupID,
+            DistrictName,
+            SiteName,
+            GroupName,
+			RosterGroupID,
+			RosterDistrictID
+        from
+            OAF_SHARED_DIMENSIONS.dbo.DimGroup
+        where
+            isDeleted = 0
+    ) geo
+where
+    geo.RosterDistrictID = cls.staging_VerticalRepayments.RosterDistrictID
+    and geo.RosterGroupID = staging_VerticalRepayments.RosterGroupID
+    and cls.staging_VerticalRepayments.DimCountryID is null
 
 
-	Update cls.staging_VerticalRepayments
-		set DimSeasonID=s.SeasonID,
-	from (
-		select CountryID, SeasonID,RosterSeasonID, SeasonName
-		from OAF_SHARED_DIMENSIONS.dbo.DimSeasons 
-		where RosterSeasonID is not null  and isDeleted=0
-		) s
-	where s.SeasonName=cls.staging_VerticalRepayments.SeasonName
-	and s.CountryID=cls.staging_VerticalRepayments.DimCountryID
-	and cls.staging_VerticalRepayments.DimSeasonID is null
 
 
-	update cls.staging_VerticalRepayments
-		set DimClientID=client.DimClientID,
-	from (
-		SELECT  cs.GlobalClientID
-		from OAF_SHARED_DIMENSIONS.dbo.DimRosterClient cs
-	) client
-	where client.GlobalClientID=cls.staging_VerticalRepayments.GlobalClientID 
-	and cls.staging_VerticalRepayments.DimClientID is null
-
+Update
+    cls.staging_VerticalRepayments
+set
+    DimSeasonID = s.SeasonID
+from
+    (
+        select
+            CountryID,
+            SeasonID,
+            RosterSeasonID,
+            SeasonName
+        from
+            OAF_SHARED_DIMENSIONS.dbo.DimSeasons
+        where
+            RosterSeasonID is not null
+            and isDeleted = 0
+    ) s
+where
+    s.RosterSeasonID = cls.staging_VerticalRepayments.RosterSeasonID
+    and s.CountryID = cls.staging_VerticalRepayments.DimCountryID
+    and cls.staging_VerticalRepayments.DimSeasonID is null
+update
+    cls.staging_VerticalRepayments
+set
+    DimClientID = client.DimClientID
+from
+    (
+        SELECT
+            cs.GlobalClientID,
+            cs.ClientID DimClientID
+        from
+            OAF_SHARED_DIMENSIONS.dbo.DimRosterClient cs
+    ) client
+where
+    cast(client.GlobalClientID as nvarchar(150)) = cls.staging_VerticalRepayments.GlobalClientID
+    and cls.staging_VerticalRepayments.DimClientID is null
 */
 
 
